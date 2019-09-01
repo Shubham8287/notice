d09b0992b939eb72160cfb209c4b89d99e2c6156
Pavan Rajesh Gandhi <pavangandhi100@gmail.com>
2019-09-01 08:39:56 -0700

    Add files via upload
    added

--- index.php
+++ index.php
@@ -7,71 +7,56 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
 	<title>Digital Notice Board</title>
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script async src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-	  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-	<link rel="stylesheet" href="adminStyle.css">
+	<script async src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script async src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
 	<link rel="stylesheet" href="style.css">
 	
 </head>
+
 <body>
 	<?$result= mysqli_query($connection,$cmd);
-	<script type="text/javascript">
-		$(function()
-		{
-
-		$("#m").attr("src","uploads/du-logo.png");
-
-
-
-		});
-	</script>
 
 	<div class="main-class ">
 	
-		<div class="main-head col-12 my-2 p-2">
+		<header class="main-head col-12 mb-4 p-3">
 			<h1>Ramanujan College - Digital Notice Board</h1>
-		</div>
+		</header>
 
 		<!-- ROW 1 -->
 
-		<div class="row mb-3 d-flex justify-content-center">
+		<div class="row mb-4 d-flex justify-content-center">
 
 			<!-- Teacher Absent Section -->
 
 			<div class="sec col-5 mr-4">
-				<div class="sec-heading photos-videos m-2">
-					<h5>Photos/Videos</h5>
+				<div class="sec-heading photos-videos">
+					<h3>Photo Gallery</h3>
 				</div>
-				<div >
-					
-					<img  id ="im" width="220px" src="getData.php" alt="">
-				</div> 
 			</div>
 
 			<!-- Teacher Absent Section -->
 
 			<div class="sec col-5 ml-4 ">
-				<div class="sec-heading teacher-absent m-2">
-					<h5>Teacher Absent</h5>
+
+				<div class="sec-heading teacher-absent">
+					<h3>Teacher Absent</h3>
 				</div>
 				<div class="row d-flex justify-content-center">
 					
-					<?php $cmd="SELECT * FROM attendance; ";
-					$result= mysqli_query($connection,$cmd);
+				<?php $cmd = "SELECT * FROM attendance;";
+				$result = mysqli_query($connection,$cmd);
 
-				while($row=mysqli_fetch_array($result))	
+				while($row = mysqli_fetch_array($result))	
 				{
        			 
         		?>
-					<div class="mx-2 teacher-absent-name col-5">
-						<?php  echo $row['name']; ?>
-					</div><?php } ?>
+				<div class="mx-1 mb-2 teacher-absent-name col-5">
+					<h5><?php echo $row['name']; ?></h5>
+				</div><?php } ?>
 
 				</div>
 			</div>
@@ -80,45 +65,48 @@
 
 		<!-- ROW 2 -->
 
-		<div class="row mb-3 d-flex justify-content-center">
+		<div class="row mb-2 d-flex justify-content-center">
 
 			<!-- Imp Notification Section -->
 
 			<div class="sec col-5 mr-4">
-				<div class="sec-heading imp-notification m-2">
-					<h5>Imp Notification</h5>
+				<div class="sec-heading imp-notification">
+					<h3>Imp Notification</h3>
 				</div>
-				<div class="col-12 text-danger">
-					<?php $cmd="SELECT * FROM alert ";
+				<div class="col-12">
+					<?php $cmd="SELECT * FROM alert
+					ORDER BY id DESC";
 					$result= mysqli_query($connection,$cmd);
 
-				while($row=mysqli_fetch_array($result))	
-				{
+					while($row=mysqli_fetch_array($result))	
+					{
        			 
-        		?>
-						<div class="imp-notification-border">
+        			?>
+					<div class="my-2 imp-notification-border">
 						<h6><?php  echo $row['description']; ?></h6>
 					</div>
-					<?php }?>
+						<?php }?>
 				</div>
+
 			</div>
 
 			<!-- Notification Section -->
 
 			<div class="sec col-5 ml-4">
-				<div class="sec-heading notification m-2">
-					<h5>Notification</h5>
+				<div class="sec-heading notification">
+					<h3>Notification</h3>
 				</div>
 				<div id="test">
 				<div class="col-12">
-					<?php $cmd="SELECT * FROM notification ";
+					<?php $cmd="SELECT * FROM notification
+					ORDER BY id DESC ";
 					$result= mysqli_query($connection,$cmd);
 
 				while($row=mysqli_fetch_array($result))	
 				{
        			 
         		?>
-					<div class="notification-border">
+					<div class="my-2 notification-border">
 						<h6><?php  echo $row['description']; ?>.</h6>
 					</div><?php } ?>
 					
@@ -127,11 +115,7 @@
 			</div>
 
 		</div>
-
-
-
-
 	</div>
-	
+	<script src="myScript.js" type="text/javascript"></script>
 </body>
 </html>
